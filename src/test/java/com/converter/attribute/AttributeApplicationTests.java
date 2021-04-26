710964d03d805eb2db67302f2bee14ec8719e934
Musa Dabra <mdabra@swglobal.local>
2021-04-26 14:46:34 +0100

    First Commit

---
+++ src/test/java/com/converter/attribute/AttributeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.converter.attribute;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AttributeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
